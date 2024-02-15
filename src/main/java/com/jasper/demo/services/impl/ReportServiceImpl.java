70a0388e76afd9d2c49e593f6efaeec432930ab6
emiliano.echinofora <emiliano.echinofora@gmail.com>
2024-02-15 23:08:51 +0000

    Jasper ReportServiceImpl.java modified

--- src/main/java/com/jasper/demo/services/impl/ReportServiceImpl.java
+++ src/main/java/com/jasper/demo/services/impl/ReportServiceImpl.java
@@ -2,10 +2,7 @@ package com.jasper.demo.services.impl;
 
 import com.jasper.demo.dto.JasperResponse;
 import com.jasper.demo.services.ReportService;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
@@ -36,41 +33,13 @@ public class ReportServiceImpl implements ReportService {
         try (Connection connection=dataSource.getConnection()){
             JasperPrint jasperPrint = JasperFillManager.fillReport(
                     jasperReport, null,connection);
-         byte[] document=exportInPdf(jasperPrint) ;
+         byte[] document=JasperExportManager.exportReportToPdf(jasperPrint); ;
          jasperResponse=new JasperResponse("world.pdf", MediaType.APPLICATION_PDF_VALUE, document);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } catch (JRException e) {
             throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
         return  jasperResponse;
-    }
-
-
-    private byte[] exportInPdf(JasperPrint jasperPrint) throws JRException, IOException {
-        byte[] retDoc=null;
-        try(ByteArrayOutputStream outputStream=new ByteArrayOutputStream()) {
-            JRPdfExporter exporter = new JRPdfExporter();
-            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-            exporter.setExporterOutput(
-                    new SimpleOutputStreamExporterOutput(outputStream));
-            SimplePdfReportConfiguration reportConfig
-                    = new SimplePdfReportConfiguration();
-            reportConfig.setSizePageToContent(true);
-            reportConfig.setForceLineBreakPolicy(false);
-
-            SimplePdfExporterConfiguration exportConfig
-                    = new SimplePdfExporterConfiguration();
-            exportConfig.setMetadataAuthor("Emiliano");
-            exportConfig.setEncrypted(true);
-            exportConfig.setAllowedPermissionsHint("PRINTING");
-            exporter.setConfiguration(reportConfig);
-            exporter.setConfiguration(exportConfig);
-            exporter.exportReport();
-            retDoc=outputStream.toByteArray();
-        }
-        return retDoc;
     }
 }
